51f500b57a757d8c4d9ff4f4fc97e2f8cbe58907
oliver-do-santos <114896426+oliver-do-santos@users.noreply.github.com>
2022-10-05 08:08:18 -0400

    clase 1: dino

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,5 +1,5 @@
-import pygame
-
+import pygame 
+from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
@@ -10,6 +10,7 @@ class Game:
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
+        self.player = Dinosaur()
         self.playing = False
         self.game_speed = 20
         self.x_pos_bg = 0
@@ -30,12 +31,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
